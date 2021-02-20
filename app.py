9237f9d2cd03b3f8eb066a7a428c669559305132
farazahmadkhan15 <farazahmadkhan15@gmail.com>
2021-02-20 13:39:47 +0500

    commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from pyfladesk import init_gui
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/timeline')
+def timeline():
+    return render_template('timeline.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    init_gui(app,
+             window_title="Eng@ge", icon="appicon.png", argv=None)
