3ade6ee7264c8e83151caade4f80083c7365a974
farazahmadkhan15 <farazahmadkhan15@gmail.com>
2021-02-22 17:19:19 +0500

    interface

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,6 @@
-# from flask_wtf import FlaskForm
-# from wtforms import StringField,PasswordField,BooleanField, SubmitField
-# from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm
+from wtforms import SelectField
+from wtforms.validators import DataRequired
 
-# class LoginForm(FlaskForm):
-#     username = StringField('Username',validators=[DataRequired()])
-#     password = PasswordField('Password',validators = [DataRequired()])
-#     remember_me = BooleanField('Remember Me')
-#     submit = SubmitField('Sign In')
+class If_form(FlaskForm):
+    interface = SelectField("Please select Interface",choices=[])
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,39 @@
-from flask import render_template,flash,redirect,url_for
+from flask import render_template,flash,redirect,url_for,request
 from flask import request
+import netifaces
+import json
+from app.forms import If_form
 from app import app
+
+
  
 
 
 
-@app.route('/')           # decorators that modifies function follows
+@app.route('/',methods=['POST','GET'])           # decorators that modifies function follows
 def index():
-       return render_template('interface.html')
+       interface = netifaces.interfaces()
+       form = If_form();
+       interface.insert(0,"-- Please Select Interface --")
+       form.interface.choices = interface
+       form.interface.default = interface[0]
+       return render_template('interface.html', form = form)
+       
+
+
+@app.route('/start',methods=['POST','GET'])           # decorators that modifies function follows
+def start():
+       form = If_form();
+       addrs = netifaces.ifaddresses(form.interface.data)
+       addrs = addrs[netifaces.AF_INET]
+       addrs = addrs[0]
+       print(addrs)
+
+
+       if form.validate_on_submit() and  form.interface.data !=  "-- Please Select Interface --":
+             return "<h1> Submit {}  </h1>".format(form.interface.data) 
+              
+       return render_template("start.html",interface=form.interface.data,addrs = addrs)
+
        
        
